c1e9ea9f10b3505033fcdd67fb7aff02247fe142
kchagappa023 <chagappa.k@unifo.in>
2026-01-20 07:10:05 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const app = express()
+
+const port = 8080
+app.listen(port, () => {
+    console.log("port running on:", port)
+})
+
+app.use("/user", (req, res, next) => {
+    let namew = "chagappa"
+    console.log(req.query.name)
+    if(namew === req.query.name){
+        next()
+    } else {
+        res.send("Authontication Failed").code(400)
+    }
+})
+
+app.get("/user/getUser", (req, res) => {
+    console.log("users")
+    res.send("nameee")
+    
+})
